f35002ddd3280e89129ec9d010392897685204cb
danny <danny@92b03d88-e023-0410-a4c4-f85a0f8b441d>
2010-12-03 11:42:38 +0000

    Initial import.

---
+++ src/main/scala/org/migror/tools/Migror.scala
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2010 Danny Lagrouw
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ */
+package org.migror.tools
+
+import org.migror.model.Migration
+
+/**
+ * Standalone tool to start a Migration.
+ */
+class Migror(migration: Migration) {
+
+  def execute: Unit = migration.execute
+
+  def this(migrationClassName: String) = this(java.lang.Class.forName(migrationClassName).newInstance.asInstanceOf[Migration])
+
+}
+
+object Migror {
+
+  def startFromCommandLine(args: Array[String]): Int = {
+    var returnCode = 1
+    try {
+      if (args.length == 1) {
+        new Migror(args(0)).execute
+        returnCode = 0
+      } else {
+        System.out.println("Geef classnaam van migratielezer als parameter")
+      }
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    return returnCode
+  }
+
+  def main(args: Array[String]): Unit = {
+    var returnCode = startFromCommandLine(args)
+    System.exit(returnCode);
+  }
+}
---
+++ src/test/scala/org/migror/tools/MigrorSpec.scala
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2010 Danny Lagrouw
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ */
+package org.migror.tools
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+import org.migror.model.{Step, Migration}
+
+class MockStep extends Step {
+  var executed = 0
+
+  def execute {
+    executed = executed + 1
+  }
+}
+
+class MockMigration extends Migration {
+  val steps = List(new MockStep)
+}
+
+class MockFailingMigration extends Migration {
+  val steps = List.empty[MockStep]
+
+  override def execute: Unit = {
+    throw new RuntimeException
+  }
+}
+
+class MigrorSpec extends Spec with ShouldMatchers {
+  describe("A Migror") {
+    it("should accept a Migration") {
+      new Migror(new MockMigration) should not be (null)
+    }
+    describe("when passed a Migration") {
+      val migration = new MockMigration
+      it("should execute all steps in that Migration") {
+        new Migror(migration).execute
+        (migration.steps(0).executed) should be (1)
+      }
+    }
+    describe("when started from the command line") {
+      val returnCode = Migror.startFromCommandLine(Array(classOf[MockMigration].getName))
+      it("should return code 0") {
+        returnCode should be (0)
+      }
+    }
+    describe("when started from the command line with an invalid class name") {
+      val returnCode = Migror.startFromCommandLine(Array("does.not.Exist"))
+      it("should return code 1") {
+        returnCode should be (1)
+      }
+    }
+    describe("when started from the command line and the Migration fails") {
+      val returnCode = Migror.startFromCommandLine(Array(classOf[MockFailingMigration].getName))
+      it("should return code 1") {
+        returnCode should be (1)
+      }
+    }
+  }
+
+}
